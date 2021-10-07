61d925e8c8c23638129fe1eb02cf24fe1a182f70
sushiAlii <aliasgar.laut@gmail.com>
2021-10-07 12:09:52 +0800

    Created daily SLP function

--- pages/scholars/_id.vue
+++ pages/scholars/_id.vue
@@ -65,30 +65,43 @@
     created() {
       this.getUserAxies()
       this.getUserInfo()
+      this.getDailySLP()
     },
     methods: {
       async getUserAxies() {
-          await this.$axios.$get('https://axie-infinity.p.rapidapi.com/get-axies/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3', {
-              headers: {
-                  'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-                  'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
-              }
-          }).then((response) => {
-              console.log("Axies",response.data.axies.results)
-          })
+        await this.$axios.$get('https://axie-infinity.p.rapidapi.com/get-axies/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3', {
+            headers: {
+                'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
+                'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
+            }
+        }).then((response) => {
+            console.log("Axies",response.data.axies.results)
+        })
       },
       async getUserInfo() {
-          await this.$axios.$get('https://axie-infinity.p.rapidapi.com/get-update/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3', {
-              headers: {
-                  'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-                  'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
-              }
-          }).then((response) => {
-              console.log("SLP",response)
-              this.slp = response.slp.total
-              this.mmr = response.leaderboard.elo
-          })
-      }
+        await this.$axios.$get('https://axie-infinity.p.rapidapi.com/get-update/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3', {
+            headers: {
+                'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
+                'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
+            }
+        }).then((response) => {
+            console.log("SLP",response)
+            this.slp = response.slp.total
+            this.mmr = response.leaderboard.elo
+        })
+      },
+      getDailySLP(){
+
+        // const claimedDate = this.$moment.unix(1630951700).format("MM/DD/YYYY")
+        const claimedDate = 1630951700
+        const currentDate = new Date().getTime()
+        const dateDiff = (currentDate - claimedDate)
+
+        console.log(claimedDate)
+        console.log(dateDiff)
+
+
+      },
     }
   }
 </script>
