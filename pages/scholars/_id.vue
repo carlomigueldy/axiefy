c495df07ba9f51f670545c1eaa37851db9e632dd
sushiAlii <aliasgar.laut@gmail.com>
2021-10-07 16:34:47 +0800

    Creating User SLP Cards, and User Axie Cards

--- pages/scholars/_id.vue
+++ pages/scholars/_id.vue
@@ -9,23 +9,150 @@
         >
             <v-card
               min-height="70"
+              
+            >
+              <v-card-title>
+                <v-row
+                  class="pt-5 px-5"
+                  align="center"
+                  justify="center"
+                >
+              
+                  <v-img
+                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                      max-height="35"
+                      max-width="35"
+                  ></v-img>
+                  <div class="headline-1 mx-3">
+                    {{ slp }}
+                  </div>
+                </v-row>
+              </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>Total SLP</v-card-title>
+                </v-row>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            md="2"
+            sm="2"
+        >
+            <v-card
+              min-height="70"
+              
+            >
+              <v-card-title>
+                <v-row
+                  class="pt-5 px-5"
+                  align="center"
+                  justify="center"
+                >
+              
+                  <v-img
+                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                      max-height="35"
+                      max-width="35"
+                  ></v-img>
+                  <div class="headline-1 mx-3">
+                    {{ slp }}
+                  </div>
+                </v-row>
+              </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>In Game SLP</v-card-title>
+                </v-row>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            md="2"
+            sm="2"
+        >
+            <v-card
+              min-height="70"
+              
+            >
+              <v-card-title>
+                <v-row
+                  class="pt-5 px-5"
+                  align="center"
+                  justify="center"
+                >
+              
+                  <v-img
+                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                      max-height="35"
+                      max-width="35"
+                  ></v-img>
+                  <div class="headline-1 mx-3">
+                    0
+                  </div>
+                </v-row>
+              </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>Ronin SLP</v-card-title>
+                </v-row>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            md="2"
+            sm="2"
+        >
+            <v-card
+              min-height="70"
+              
             >
                 <v-card-title>
                   <v-row
-                      align="center"
+                    class="pt-5 px-5"
+                    align="center"
+                    justify="center"
                   >
-                    <v-col>
                       <v-img
                           src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
-                          max-height="40"
-                          max-width="40"
+                          max-height="35"
+                          max-width="35"
                       ></v-img>
-                    </v-col>
-                    <v-col>
-                        {{ slp }} SLP
-                    </v-col>
+                    <div class="headline-1 mx-3">
+                      {{ dailySLP }} 
+                    </div>
                   </v-row>
                 </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>Daily Average</v-card-title>
+                </v-row>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            md="2"
+            sm="2"
+        >
+            <v-card
+              min-height="70"
+              
+            >
+                <v-card-title>
+                  <v-row
+                    class="pt-5 px-5"
+                    align="center"
+                    justify="center"
+                  >
+                      <v-img
+                          src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                          max-height="35"
+                          max-width="35"
+                      ></v-img>
+                    <div class="headline-1 mx-3">
+                      {{ farmedToday }} 
+                    </div>
+                  </v-row>
+                </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>Farmed Today</v-card-title>
+                </v-row>
             </v-card>
         </v-col>
         <v-col
@@ -38,17 +165,60 @@
             >
                 <v-card-title>
                   <v-row
-                      align="center"
+                    class="pt-5 px-5"
+                    align="center"
+                    justify="center"
                   >
-                    <v-col>
-                      ELO
-                    </v-col>
-                    <v-col>
-                        {{ mmr }} MMR
-                    </v-col>
+                    <v-img
+                      class="rounded-circle"
+                      src="https://chimeratribune.com/wp-content/uploads/2020/11/axieareanaswords.png"
+                      max-height="35"
+                      max-width="35"
+                    ></v-img>
+                    <div class="headline-1 mx-3">
+                      {{ mmr }}
+                    </div>
                   </v-row>
                 </v-card-title>
+                <v-row justify="center">
+                  <v-card-title>Current MMR</v-card-title>
+                </v-row>
             </v-card>
+        </v-col>
+      </v-row>
+      <v-divider class="mt-10"></v-divider>
+      <div class="mt-5">
+        <h2>
+        {{axies.length}} Axies
+        </h2>
+      </div>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          md="2"
+          v-for="axie in axies"
+          :key="axie.id"
+        >
+          <v-card>
+            <v-card-subtitle>
+              Axie #{{ axie.id }}
+            </v-card-subtitle>
+            <v-card-text
+              align="center"
+              justify="center"
+            >
+              <v-img
+                :src="axie.image"
+                max-height="250"
+                max-width="250"
+              >
+              </v-img>
+            </v-card-text>
+            
+          </v-card>
         </v-col>
       </v-row>
   </app-main-container>
@@ -59,12 +229,16 @@
     data() {
       return {
         slp: "--",
+        dailySLP: "--",
+        farmedToday: "--",
+        lastClaimedSLP: 0,
         mmr: "--",
+        axies: []
       }
     },
-    created() {
-      this.getUserAxies()
-      this.getUserInfo()
+    async created() {
+      await this.getUserAxies()
+      await this.getUserInfo()
       this.getDailySLP()
     },
     methods: {
@@ -76,6 +250,7 @@
             }
         }).then((response) => {
             console.log("Axies",response.data.axies.results)
+            this.axies = response.data.axies.results
         })
       },
       async getUserInfo() {
@@ -87,19 +262,35 @@
         }).then((response) => {
             console.log("SLP",response)
             this.slp = response.slp.total
+            this.lastClaimedSLP = response.slp.lastClaimedItemAt
+            this.farmedToday = response.slp.todaySoFar
             this.mmr = response.leaderboard.elo
         })
+        // await this.$axios.$get('https://game-api.axie.technology/api/v1/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3'), {
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //     'Authorization': 'Bearer ' + token
+        //   }
+        // }
+        // .then((response) => {
+        //     console.log("NEW API",response)
+        // })
       },
       getDailySLP(){
+        //  Date received from API
+        const claimedDate = this.lastClaimedSLP
+        const currentDate = Math.floor(new Date().getTime()/1000.0)
+        const dateDiff = currentDate - claimedDate
+        let daysDiff = dateDiff / (3600 * 24)
 
-        // const claimedDate = this.$moment.unix(1630951700).format("MM/DD/YYYY")
-        const claimedDate = 1630951700
-        const currentDate = new Date().getTime()
-        const dateDiff = (currentDate - claimedDate)
+        console.log("Inputed",claimedDate)
+        console.log(currentDate)
+        console.log(Math.floor(daysDiff) + " days")
 
-        console.log(claimedDate)
-        console.log(dateDiff)
-
+        this.dailySLP = Math.ceil(this.slp / daysDiff)
+        console.log(this.slp)
+        console.log(daysDiff)
+        console.log(this.dailySLP)
 
       },
     }
