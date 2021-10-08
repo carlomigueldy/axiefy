7465282b6656f2614a5011fc337e3e65a7ee111d
sushiAlii <aliasgar.laut@gmail.com>
2021-10-08 09:01:12 +0800

    Changed Formatter to Prettier

--- pages/scholars/_id.vue
+++ pages/scholars/_id.vue
@@ -1,314 +1,225 @@
 <template>
   <app-main-container>
     <template v-slot:title>{{ $route.params.id }}</template>
-      <v-row>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-              
-            >
-              <v-card-title>
-                <v-row
-                  class="pt-5 px-5"
-                  align="center"
-                  justify="center"
-                >
-              
-                  <v-img
-                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
-                      max-height="35"
-                      max-width="35"
-                  ></v-img>
-                  <div class="headline-1 mx-3">
-                    {{ slp.total }}
-                  </div>
-                </v-row>
-              </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>Total SLP</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-              
-            >
-              <v-card-title>
-                <v-row
-                  class="pt-5 px-5"
-                  align="center"
-                  justify="center"
-                >
-              
-                  <v-img
-                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
-                      max-height="35"
-                      max-width="35"
-                  ></v-img>
-                  <div class="headline-1 mx-3">
-                    {{ slp.inGameSLP }}
-                  </div>
-                </v-row>
-              </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>In Game SLP</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-              
-            >
-              <v-card-title>
-                <v-row
-                  class="pt-5 px-5"
-                  align="center"
-                  justify="center"
-                >
-              
-                  <v-img
-                      src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
-                      max-height="35"
-                      max-width="35"
-                  ></v-img>
-                  <div class="headline-1 mx-3">
-                    {{ slp.roninSLP }}
-                  </div>
-                </v-row>
-              </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>Ronin SLP</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-              
-            >
-                <v-card-title>
-                  <v-row
-                    class="pt-5 px-5"
-                    align="center"
-                    justify="center"
-                  >
-                      <v-img
-                          src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
-                          max-height="35"
-                          max-width="35"
-                      ></v-img>
-                    <div class="headline-1 mx-3">
-                      {{ slp.dailySLP }} 
-                    </div>
-                  </v-row>
-                </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>Daily Average</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-              
-            >
-                <v-card-title>
-                  <v-row
-                    class="pt-5 px-5"
-                    align="center"
-                    justify="center"
-                  >
-                      <v-img
-                        class="rounded-circle"
-                        src="https://chimeratribune.com/wp-content/uploads/2020/11/axieareanaswords.png"
-                        max-height="35"
-                        max-width="35"
-                      ></v-img>
-                    <div class="headline-1 mx-3">
-                      {{ rank }} 
-                    </div>
-                  </v-row>
-                </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>Current Rank</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-        <v-col
-            cols="12"
-            md="2"
-            sm="2"
-        >
-            <v-card
-              min-height="70"
-            >
-                <v-card-title>
-                  <v-row
-                    class="pt-5 px-5"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-img
-                      class="rounded-circle"
-                      src="https://chimeratribune.com/wp-content/uploads/2020/11/axieareanaswords.png"
-                      max-height="35"
-                      max-width="35"
-                    ></v-img>
-                    <div class="headline-1 mx-3">
-                      {{ mmr }}
-                    </div>
-                  </v-row>
-                </v-card-title>
-                <v-row justify="center">
-                  <v-card-title>Current MMR</v-card-title>
-                </v-row>
-            </v-card>
-        </v-col>
-      </v-row>
-      <v-divider class="mt-10"></v-divider>
-      <div class="mt-5">
-        <h2>
-        {{axies.length}} Axies
-        </h2>
-      </div>
-      <v-row
-        align="center"
-        justify="center"
-      >
-        <v-col
-          cols="12"
-          md="2"
-          v-for="axie in axies"
-          :key="axie.id"
-        >
-          <v-card>
-            <v-card-subtitle>
-              #{{ axie.id }}
-            </v-card-subtitle>
-            <v-card-text
-              align="center"
-              justify="center"
-            >
+    <v-row>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
               <v-img
-                :src="axie.image"
-                max-height="250"
-                max-width="250"
-              >
-              </v-img>
-            </v-card-text>
-            
-          </v-card>
-        </v-col>
-      </v-row>
+                src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ slp.total }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>Total SLP</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
+              <v-img
+                src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ slp.inGameSLP }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>In Game SLP</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
+              <v-img
+                src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ slp.roninSLP }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>Ronin SLP</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
+              <v-img
+                src="https://assets.coingecko.com/coins/images/10366/large/SLP.png?1578640057"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ slp.dailySLP }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>Daily Average</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
+              <v-img
+                class="rounded-circle"
+                src="https://chimeratribune.com/wp-content/uploads/2020/11/axieareanaswords.png"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ rank }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>Current Rank</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2" sm="2">
+        <v-card min-height="70">
+          <v-card-title>
+            <v-row class="pt-5 px-5" align="center" justify="center">
+              <v-img
+                class="rounded-circle"
+                src="https://chimeratribune.com/wp-content/uploads/2020/11/axieareanaswords.png"
+                max-height="35"
+                max-width="35"
+              ></v-img>
+              <div class="headline-1 mx-3">
+                {{ mmr }}
+              </div>
+            </v-row>
+          </v-card-title>
+          <v-row justify="center">
+            <v-card-title>Current MMR</v-card-title>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider class="mt-10"></v-divider>
+    <div class="mt-5">
+      <h2>{{ axies.length }} Axies</h2>
+    </div>
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="2" v-for="axie in axies" :key="axie.id">
+        <v-card>
+          <v-card-subtitle> #{{ axie.id }} </v-card-subtitle>
+          <v-card-text align="center" justify="center">
+            <v-img :src="axie.image" max-height="250" max-width="250"> </v-img>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </app-main-container>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
-  export default{
-    data() {
-      return {
-        slp: {
-          total: "--",
-          roninSLP: "--",
-          inGameSLP: "--",
-          dailySLP: "--",
-          farmedToday: "--",
-          lastClaimedSLP: 0,
-        },
-        mmr: "--",
-        rank: "--",
-        axies: []
+export default {
+  data() {
+    return {
+      slp: {
+        total: "--",
+        roninSLP: "--",
+        inGameSLP: "--",
+        dailySLP: "--",
+        farmedToday: "--",
+        lastClaimedSLP: 0
+      },
+      mmr: "--",
+      rank: "--",
+      axies: []
+    };
+  },
+  async created() {
+    await this.getUserAxies();
+    await this.getUserInfo();
+    // await this.getSLPToday()
+    this.getDailySLP();
+  },
+  methods: {
+    async getUserAxies() {
+      try {
+        const response = await this.$axios.$get(
+          "https://axie-infinity.p.rapidapi.com/get-axies/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3",
+          {
+            headers: {
+              "x-rapidapi-host": "axie-infinity.p.rapidapi.com",
+              "x-rapidapi-key":
+                "b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd"
+            }
+          }
+        );
+        console.log("AXIES", response.data.axies.results);
+        this.axies = response.data.axies.results;
+      } catch (error) {
+        console.log(error);
       }
     },
-    async created() {
-      await this.getUserAxies()
-      await this.getUserInfo()
-      // await this.getSLPToday()
-      this.getDailySLP()
+    async getUserInfo() {
+      const response = await axios.get(
+        "https://game-api.axie.technology/api/v1/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3",
+        { accept: "application/json" }
+      );
+      console.log("NEW API", response);
+      this.slp.total = response.data.total_slp;
+      this.slp.inGameSLP = response.data.in_game_slp;
+      this.slp.roninSLP = response.data.ronin_slp;
+      this.slp.lastClaimedSLP = response.data.last_claim;
+      this.mmr = response.data.mmr;
+      this.rank = response.data.rank;
+
+      // const response2 = await axios.get(
+      //   "https://axie-infinity.p.rapidapi.com/get-update/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3", {
+      //     headers: {
+      //       'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
+      //       'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
+      //     }
+      //   });
+      //   console.log("RESPONSE 2",response2)
     },
-    methods: {
-      async getUserAxies() {
-        try{
-          const response = await this.$axios.$get('https://axie-infinity.p.rapidapi.com/get-axies/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3', {
-            headers: {
-                  'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-                  'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
-              }
-          });
-          console.log("AXIES",response.data.axies.results)
-          this.axies = response.data.axies.results
+    getDailySLP() {
+      //  Date received from API
+      const claimedDate = this.slp.lastClaimedSLP;
+      const currentDate = Math.floor(new Date().getTime() / 1000.0);
+      const dateDiff = currentDate - claimedDate;
+      let daysDiff = dateDiff / (3600 * 24);
 
+      console.log("Inputed", claimedDate);
+      console.log(currentDate);
+      console.log(Math.floor(daysDiff) + " days");
 
-        }catch(error){
-          console.log(error)
-        }
-      },
-      async getUserInfo() {
-        const response = await axios.get(
-          "https://game-api.axie.technology/api/v1/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3",
-          { accept: "application/json" }
-        );
-          console.log("NEW API",response)
-          this.slp.total = response.data.total_slp
-          this.slp.inGameSLP = response.data.in_game_slp
-          this.slp.roninSLP = response.data.ronin_slp
-          this.slp.lastClaimedSLP = response.data.last_claim
-          this.mmr = response.data.mmr
-          this.rank = response.data.rank
-
-        // const response2 = await axios.get(
-        //   "https://axie-infinity.p.rapidapi.com/get-update/0xc20dabcad7bf3971fb11e89bf50bf2ff6dec0fd3", {
-        //     headers: {
-        //       'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-        //       'x-rapidapi-key': 'b5e6783431msh5dfe6faf063f956p16dfadjsnab843e1e67cd'
-        //     }
-        //   });
-        //   console.log("RESPONSE 2",response2)
-
-      },
-      getDailySLP(){
-        //  Date received from API
-        const claimedDate = this.slp.lastClaimedSLP
-        const currentDate = Math.floor(new Date().getTime()/1000.0)
-        const dateDiff = currentDate - claimedDate
-        let daysDiff = dateDiff / (3600 * 24)
-
-        console.log("Inputed",claimedDate)
-        console.log(currentDate)
-        console.log(Math.floor(daysDiff) + " days")
-
-        this.slp.dailySLP = Math.ceil(this.slp.total / daysDiff)
-        console.log(this.slp)
-        console.log(daysDiff)
-        console.log(this.slp.dailySLP)
-
-      },
+      this.slp.dailySLP = Math.ceil(this.slp.total / daysDiff);
+      console.log(this.slp);
+      console.log(daysDiff);
+      console.log(this.slp.dailySLP);
     }
   }
+};
 </script>
 
 <style></style>
