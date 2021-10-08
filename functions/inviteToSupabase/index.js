566dd1ba173d8c6dadde0a0309aa8a074ae7d2c6
carlomigueldy <carlomigueldy@gmail.com>
2021-10-08 22:10:34 +0800

    Finalize lambda function

--- functions/inviteToSupabase/index.js
+++ functions/inviteToSupabase/index.js
@@ -2,13 +2,26 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 exports.handler = async function(event, context) {
+  const { email } = event;
+
+  if (!email) {
+    return {
+      code: 400,
+      message:
+        "The `email` field is required and must be a valid email address."
+    };
+  }
+
   const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 
-  const response = await supabase.auth.api.inviteUserByEmail(
-    "carlomigueldy@gmail.com"
-  );
+  const response = await supabase.auth.api.inviteUserByEmail(email);
   console.log(response);
+
+  return {
+    code: 200,
+    message: `Invitation sent to ${email}.`
+  };
 };
