941b92940ec90d287dbc855e4762147f87e12bac
carlomigueldy <carlomigueldy@gmail.com>
2021-10-09 21:29:20 +0800

    Add GIF

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>{{ $route.query }}</div>
+  <div>
+    <iframe
+      src="https://giphy.com/embed/Sqa4huGTDwJZNtvMky"
+      width="480"
+      height="268"
+      frameBorder="0"
+      class="giphy-embed"
+      allowFullScreen
+    ></iframe>
+    <p>
+      <a
+        href="https://giphy.com/gifs/ctvcomedy-lights-out-david-spade-call-back-Sqa4huGTDwJZNtvMky"
+      >
+        via GIPHY
+      </a>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  created() {
-    console.log("this.$route.query =>", this.$route.query);
+  async created() {
+    if (Object.keys(this.$route.query).length !== 0) {
+      const { access_token, refresh_token } = this.$route.query;
+
+      await this.$auth.setUserToken(access_token, refresh_token);
+      return;
+    }
+
+    const [_, queryString] = this.$route.fullPath.split("#");
+    console.log({ first: _, last: queryString });
+
+    if (queryString) {
+      const path = `${window.location.origin}/?${queryString}`;
+      window.location.replace(path);
+      return;
+    }
   }
 };
 </script>
