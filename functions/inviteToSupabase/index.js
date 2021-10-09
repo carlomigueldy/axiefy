1b93b26dda1130df7295a63eee89ac2fd0129dc3
carlomigueldy <carlomigueldy@gmail.com>
2021-10-09 16:16:51 +0800

    Update function

--- functions/inviteToSupabase/index.js
+++ functions/inviteToSupabase/index.js
@@ -2,37 +2,109 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 exports.handler = async function(event, context) {
-  const { email, sender } = JSON.parse(event.body);
-
-  if (!email || !sender) {
-    return {
-      code: 400,
-      message:
-        "The `email` and `sender` field is required and both must be a valid email address."
-    };
-  }
-
   const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 
-  const { error } = await supabase.auth.api.inviteUserByEmail(email, {
-    data: {
-      inviteBy: sender
-    }
-  });
+  const { email } = JSON.parse(event?.body || "{}");
+  const headers = event?.headers;
 
-  if (error) {
+  if (!email) {
     return {
-      code: 422,
-      error,
-      message: error.message
+      code: 400,
+      message:
+        "The `email` field is required and must be a valid email address."
     };
   }
 
-  return {
-    code: 200,
-    message: `Invitation sent to ${email}.`
-  };
+  if (!headers["Authorization"]) {
+    return {
+      code: 401,
+      message: `No request header "Authorization" found.`
+    };
+  }
+
+  try {
+    const {
+      error: authError,
+      user: authUser
+    } = await supabase.auth.api.getUser(
+      headers["Authorization"].split("Bearer ")[1]
+    );
+
+    if (authError) {
+      const message = "You are not authenticated.";
+      console.error(message, authError);
+      return {
+        code: 401,
+        error: authError,
+        message
+      };
+    }
+
+    const {
+      data: inviteData,
+      error: inviteError
+    } = await supabase.auth.api.inviteUserByEmail(email, {
+      data: {
+        invited_by: authUser
+      }
+    });
+
+    if (inviteError) {
+      return {
+        code: 422,
+        error: inviteError,
+        message: inviteError.message
+      };
+    }
+
+    const { data: team, error: teamError } = await supabase
+      .from("teams")
+      .select()
+      .eq("owner_id", authUser.id)
+      .single();
+
+    if (teamError) {
+      const message = `The user did not have a Team`;
+      console.error(message, teamError);
+      return {
+        code: 500,
+        message,
+        error: teamError
+      };
+    }
+
+    const {
+      data: teamMemberData,
+      error: teamMemberError
+    } = await supabase.from("team_members").insert({
+      user_id: inviteData.id,
+      team_id: team.id
+    });
+
+    if (teamMemberError) {
+      const message = `Error ocurred when inserting a record for "team_members"`;
+      console.error(message, teamMemberError);
+      return {
+        code: 500,
+        message,
+        error: teamMemberError
+      };
+    }
+
+    return {
+      code: 200,
+      message: `Invitation sent to ${email}.`,
+      data: {
+        teamMemberData,
+        inviteData
+      }
+    };
+  } catch (error) {
+    return {
+      error
+    };
+  }
 };
