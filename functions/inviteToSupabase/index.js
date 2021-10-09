17705dc71e28ebab39efb57d117ffe83b58f0f35
carlomigueldy <carlomigueldy@gmail.com>
2021-10-09 19:21:07 +0800

    Log

--- functions/inviteToSupabase/index.js
+++ functions/inviteToSupabase/index.js
@@ -1,17 +1,17 @@
 const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
-exports.handler = async function(event, context) {
-  const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-  );
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
+exports.handler = async function(event, context) {
+  console.log({ event, context });
   const { email } = JSON.parse(event?.body || "{}");
   const headers = event.headers;
 
-  console.log(headers);
-
+  /** validate */
   if (!headers.authorization) {
     const message = `No authorization header found in request.`;
     console.error(message, { headers, email });
@@ -25,7 +25,6 @@ exports.handler = async function(event, context) {
       }
     };
   }
-
   if (!email) {
     const message =
       "The `email` field is required and must be a valid email address.";
@@ -36,15 +35,15 @@ exports.handler = async function(event, context) {
     };
   }
 
+  /** fetch JWT user */
   const [_, token] = headers.authorization.split("Bearer ");
   const { error: authError, user: authUser } = await supabase.auth.api.getUser(
     token
   );
   console.log({ token, authError, authUser });
-
   if (authError) {
     const message = "You are not authenticated.";
-    console.error(message, { authError, token, headers, email });
+    console.error(message, authError);
     return {
       code: 401,
       error: authError,
@@ -52,12 +51,14 @@ exports.handler = async function(event, context) {
     };
   }
 
+  /** invite new user by email */
   const {
     data: inviteData,
     error: inviteError
-  } = await supabase.auth.api.inviteUserByEmail(email);
+  } = await supabase.auth.api
+    .inviteUserByEmail(email)
+    .catch(error => console.error({ error }));
   console.log({ inviteData, inviteError });
-
   if (inviteError) {
     return {
       code: 422,
@@ -66,13 +67,13 @@ exports.handler = async function(event, context) {
     };
   }
 
-  const { data: team, error: teamError } = await supabase
+  /** fetch team by JWT user */
+  const { data: teamData, error: teamError } = await supabase
     .from("teams")
     .select()
     .eq("owner_id", authUser.id)
     .single();
-  console.log({ team, teamError });
-
+  console.log({ teamData, teamError });
   if (teamError) {
     const message = `The user did not have a Team`;
     console.error(message, teamError);
@@ -83,14 +84,14 @@ exports.handler = async function(event, context) {
     };
   }
 
+  /** assign new team member */
   const { data: teamMemberData, error: teamMemberError } = await supabase
     .from("team_members")
     .insert({
       user_id: inviteData.id,
-      team_id: team.id
+      team_id: teamData.id
     });
   console.log({ teamMemberData, teamMemberError });
-
   if (teamMemberError) {
     const message = `Error ocurred when inserting a record for "team_members"`;
     console.error(message, teamMemberError);
