4d7a5a2eb7d9ba05a19dd47e248a1313938d8b4e
carlomigueldy <carlomigueldy@gmail.com>
2021-10-09 08:49:18 +0800

    Set fallback to false

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  fallback: false,
+
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
