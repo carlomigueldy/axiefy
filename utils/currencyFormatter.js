97c9fb16f3d27aef36588039bacd4aeaf2809b90
sushiAlii <aliasgar.laut@gmail.com>
2021-10-10 07:43:03 +0800

    Created currencyFormatter Util

---
+++ utils/currencyFormatter.js
@@ -0,0 +1,16 @@
+export const formatUSD = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+export const formatPHP = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "PHP"
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
