3c77cf4f4dda6347e796b51a1a66d36300da968e
sushiAlii <aliasgar.laut@gmail.com>
2021-10-10 10:51:53 +0800

    Created coin gecko base URL

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,3 @@
 export const AXIE_RAPID_API_BASE_URL = "https://axie-infinity.p.rapidapi.com";
 export const AXIE_GAME_API_BASE_URL = "https://game-api.axie.technology";
+export const COIN_GECKO_API_BASE_URL = "https://api.coingecko.com";
