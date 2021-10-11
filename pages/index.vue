07148d7f854c48c6ef8ff2d576b47cc07d6339e6
sushiAlii <aliasgar.laut@gmail.com>
2021-10-11 11:31:52 +0800

    Added App title, footer on the landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,56 @@
 <template>
-  <v-sheet color="transparent" height="100vh" width="100%">
-    <app-nav-bar>
-      <div class="title">
-        App Name
-      </div>
+  <v-container>
+    <v-sheet color="transparent" height="85vh" width="100%">
+      <app-nav-bar>
+        <div class="title">
+          Axiefy BETA
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn depressed to="/auth/login">
-        Sign In
-      </v-btn>
-    </app-nav-bar>
+        <v-btn depressed to="/auth/login">
+          Sign In
+        </v-btn>
+      </app-nav-bar>
 
-    <v-row>
-      <v-col>
-        <!-- <v-card class="logo py-4 d-flex justify-center">
+      <v-row>
+        <v-col>
+          <!-- <v-card class="logo py-4 d-flex justify-center">
           <NuxtLogo />
           <VuetifyLogo />
         </v-card> -->
-        <div class="d-flex justify-center align-center" style="height: 80vh">
-          <div>
-            <div class="display-1">Powered by Supabase</div>
-            <div class="text-center my-5">
-              <v-btn
-                color="primary"
-                to="/auth/register"
-                rounded
-                x-large
-                depressed
-              >
-                Get Started
-              </v-btn>
+          <div class="d-flex justify-center align-center" style="height: 80vh">
+            <div>
+              <div class="display-1 text-center">
+                Managing Scholars Made Easy
+              </div>
+
+              <div class="text-center my-5">
+                <v-btn
+                  color="primary"
+                  to="/auth/register"
+                  rounded
+                  x-large
+                  depressed
+                >
+                  Get Started
+                </v-btn>
+              </div>
             </div>
           </div>
-        </div>
-      </v-col>
-    </v-row>
-  </v-sheet>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <v-footer class="d-flex justify-center mr-3" color="transparent">
+      <v-img
+        class="rounded-lg mr-3"
+        :src="$store.state.assets.supabase"
+        max-height="50"
+        max-width="50"
+      ></v-img>
+      <div class="headline text-center grey--text">Powered by Supabase</div>
+    </v-footer>
+  </v-container>
 </template>
 
 <script>
